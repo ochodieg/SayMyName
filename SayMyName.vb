6d8c222cb680d6e5cffabd1ce6754cf437153f42
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-30 23:22:39 -0600

    Add project files.

---
+++ SayMyName.vb
@@ -0,0 +1,21 @@
+﻿'Ivan Ochoa
+'RCET0265
+'Fall 2020, Semester 4
+'Say My Name
+
+Option Strict On
+Option Explicit On
+
+Module SayMyName
+
+    Sub Main()
+        Dim userInput As String
+        Console.WriteLine("beep boop, what is your name, human?")
+        userInput = Console.ReadLine()
+        Console.WriteLine("Ah")
+        Console.WriteLine("Well...When skynets time comes... I'll remember your name," & " " & userInput)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
